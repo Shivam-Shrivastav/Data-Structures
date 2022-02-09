7cedfece3ffdc6871d53cad9923ba499232d5747
Shivam-Shrivastav <msdj2110@gmail.com>
2022-02-09 13:53:46 +0530

    Implement Stack & Queue

---
+++ DSA in Python/Implement_Stack_from_scratch.py
@@ -0,0 +1,52 @@
+#User function Template for python3
+
+class MyStack:
+    
+    def __init__(self):
+        self.arr=[]
+    def isempty(self):
+      return len(self.arr)==0
+    
+    #Function to push an integer into the stack.
+    def push(self,data):
+        #add code here
+        self.arr.append(data)
+        
+        
+    
+    #Function to remove an item from top of the stack.
+    def pop(self):
+        #add code here
+        if self.isempty():
+            return -1
+        else:
+            popped = self.arr[-1]
+            del self.arr[-1]
+            return popped
+        
+        
+        
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        s=MyStack()
+        q=int(input())
+        q1=list(map(int,input().split()))
+        i=0
+        while(i<len(q1)):
+            if(q1[i]==1):
+                s.push(q1[i+1])
+                i=i+2
+            elif(q1[i]==2):
+                print(s.pop(),end=" ")
+                i=i+1
+            elif(s.isEmpty()):
+                print(-1)
+                i=i+1
+        print()   
+# } Driver Code Ends
---
+++ DSA in Python/Implement_queue_from_scratch.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+class MyQueue:
+    
+    def __init__(self):
+        self.arr = []
+        self.front = 0
+        self.rear = 0
+    
+    #Function to push an element x in a queue.
+    def push(self, x):
+        self.arr.append(x)
+        self.rear = self.rear + 1
+     
+    #Function to pop an element from queue and return that element.
+    def pop(self):
+        if self.front == self.rear:
+            return -1
+        elif self.front < self.rear:
+            temp = self.arr[self.front]
+            self.front = self.front + 1
+        return temp
+        
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        s=MyQueue()
+        q=int(input())
+        q1=list(map(int,input().split()))
+        i=0
+        while(i<len(q1)):
+            if(q1[i]==1):
+                s.push(q1[i+1])
+                i=i+2
+            elif(q1[i]==2):
+                print(s.pop(),end=" ")
+                i=i+1
+            elif(s.isEmpty()):
+                print(-1)
+                i=i+1
+        print()   
+
+# } Driver Code Ends
